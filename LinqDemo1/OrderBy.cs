d3357df1614aa249d0b80753c82d5910115f91cb
Chaten Mundle <chetanmundle7875@gmail.com>
2024-08-24 23:08:30 +0530

    24-08-24

--- LinqDemo1/OrderBy.cs
+++ LinqDemo1/OrderBy.cs
@@ -10,6 +10,82 @@ namespace LinqDemo1
     {
         public void OrderByMethod()
         {
+            var intData = new List<int>() { 100, 5, 12, 13, 1, 7, 53, 35 };
+            Console.WriteLine("Befor Order By : ");
+
+            intData.ForEach(num => Console.Write($"{num}\t"));
+
+            var orderByQuery = (from num in intData
+                                orderby num
+                               select num).ToList();                                 // OR
+
+            var orderByMethod = intData.OrderBy(num => num).ToList();
+
+
+            Console.WriteLine("\n After Apply Order by : ");
+            foreach(var curElm in orderByQuery)
+            {
+                Console.Write($"{curElm}\t");
+            }
+
+
+
+            Console.WriteLine("\n\n-------------------------------------------------------------------------");
+            Console.WriteLine("\n\n Data which is the integers are greater than 10 and order by : ");
+
+            var conditionQuery = (from num in intData
+                                 where num > 10
+                                 orderby num
+                                 select num).ToList();
+
+            var conditionMethod = intData.Where(num => num > 10).OrderBy(num=> num).ToList();
+
+            conditionMethod.ForEach(num => Console.Write($"{num}\t"));
+
+
+
+
+            Console.WriteLine("\n\n-------------------------------------------------------------------------");
+
+            Console.WriteLine("ORder by in string");
+            var names = new List<string>() { "Chetan", "rahul", "Aman", "Yugal", "Vinit", "Raunit" };
+
+            var stringOrderByQ = (from name in names
+                                  orderby name
+                                  select name).ToList();                                                       // OR
+
+            var stringOrderByM = names.OrderBy(name => name).ToList();
+
+            foreach(var name in stringOrderByQ)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            Console.WriteLine("\n\n-------------------------------------------------------------------------");
+
+            var dataSource = new List<Employee2>()
+            {
+               new Employee2(){Id=4,Name = "Aman",Email="c@gmail.com"},
+               new Employee2(){Id=3,Name = "Kunal",Email="c@gmail.com"},
+               new Employee2(){Id=2,Name = "Raju",Email="c@gmail.com"},
+               new Employee2(){Id=5,Name = "HArry",Email="c@gmail.com"},                
+               new Employee2(){Id=1,Name = "Chetan",Email="c@gmail.com"},
+            };
+
+            var sortByidQ = (from emp in dataSource
+                             orderby emp.Id
+                             select emp).ToList();
+
+            var sortByidM = dataSource.OrderBy(emp => emp.Id).ToList();
+
+            Console.WriteLine("Sorted Data : ");
+
+            foreach(var emp in sortByidM)
+            {
+                Console.WriteLine($"ID : {emp.Id}, Name : {emp.Name} and Email : {emp.Email}");
+            }
+
 
         }
     }
